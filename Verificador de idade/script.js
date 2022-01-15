414e854b94b216ce618ab6676680f8c606ebdeaf
Pedro Perazza <92265606+PedroidG@users.noreply.github.com>
2022-01-15 14:54:04 -0300

    Atualizando com um novo projeto

---
+++ Verificador de idade/script.js
@@ -0,0 +1,51 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente.') 
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'imagens/bebemenino.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'imagens/jovem.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'imagens/homemadulto.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/idoso.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'imagens/bebemenina.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'imagens/jovemmenina.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'imagens/mulheradulta.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
